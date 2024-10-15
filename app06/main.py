37fe57697ae6aaa20fa769a48a36390d43c9e142
KKevin19 <rojasocampokevindaniel@cetis50cdmx.com>
2024-10-14 18:37:56 -0600

    Update main.py

--- app06/main.py
+++ app06/main.py
@@ -1,48 +1,66 @@
 import flet as ft
-
-#se inporta la libreria random
 import random
 
-def main(page: ft.Page):
-    #se crean las variables globales
-    global numero_secreto,entrada_numero,text_resultado,boton_adivinar
+def verificar_adivinanza(e,page):
+    adivinanza_usuario=int(entrada_numero.value)
     
-    page.title="Adivina el numero"
-    
-    #se genera un numero aleatorio entre 1 y 100
-    numero_secreto = random.randint(1,100)
-    
-    #se generala interfaz grafico
-    titulo=ft.Text("Adivina el numero")
-    entrada_numero=ft.TextField(label="tu adivinanza entre 1 y 100",width=300)
-    boton_adivinar=ft.ElevatedButton("Adivinar")
-    text_resultado=ft.Text("",color="white")
+    if adivinanza_usuario==numero_secreto:
+        texto_resultado.value="¡felicidades! adivinaste el numero secreto"
+        boton_adivinar.disabled=True
+        page.add(ft.Audio(src="victoria.mp3",autoplay=True))
 
-#se crea el contenedor de la interfaz grafico
-contenedor_principál=ft.Container(
-    content=ft.Column(
-        controls=[
-            titulo,
-            entrada_numero,
-            boton_adivinar,
-            text_resultado,
-            ft.Image(
-                src="https://i.ibb.co/Gxgryg9/laser.gif",
-                fit=ft.ImageFit.COVER,
-                witdh=350,
-                height=300
+    elif adivinanza_usuario < numero_secreto:
+        texto_resultado.value="¡fallaste! el numero secreto es mayor"
+        page.add(ft.Audio(src="boing.mp3",autoplay=True))
+    else:
+        texto_resultado.value="¡fallaste! el numero secreto es menor"
+        page.add(ft.Audio(src="boing.mp3",autoplay=True))
+        
+    entrada_numero.value=""
+    page.update()    
+
+
+#funcion principal
+def main(page: ft.Page):
+    #variables globales
+    global numero_secreto,entrada_numero,texto_resultado,boton_adivinar
+    
+    page.title="adivina el numero"
+    
+    #generar un numero aleatorio
+    numero_secreto=random.randint(1,100)
+    
+    #crear elementos de la interfaz
+    titulo=ft.Text("adivina el numero entre 1 y 100",size=20,color="red")
+    entrada_numero=ft.TextField(label="tu adivinanza",width=150)
+    boton_adivinar=ft.ElevatedButton("adivinar", on_click=lambda e: verificar_adivinanza(e, page))
+    texto_resultado=ft.Text("",color="red")
+    
+    contenedor_principal=ft.Container(
+        content=ft.Column(
+            controls=[
+                titulo,
+                entrada_numero,
+                boton_adivinar,
+                texto_resultado,
+                ft.Image(
+                    src="https://i.ibb.co/Gxgryg9/laser.gif",
+                    fit=ft.ImageFit.COVER,
+                    width=350,
+                    height=200
                 )
-            ],alignment="CENTER",
-            horizontal_alignment="CENTER",
+            ],alignment="center",
+            horizontal_alignment="center",
             spacing=20
         ),
-        bgcolor="black",
+        bgcolor="green",
         width=page.window.width,
         height=page.window.height,
         padding=20
+            
+        
     )
-    
-page.add(contenedor_principál)
+    page.add(contenedor_principal)
 
 
 ft.app(main)
